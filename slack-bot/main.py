42ba05e0d8f6e55b145a6df93d0f9f33e59c6370
Rolv Apneseth <rolv.apneseth@gmail.com>
2021-02-22 19:28:58 +0000

    Started work on a message handling event and made helper functions for it

--- slack-bot/main.py
+++ slack-bot/main.py
@@ -9,6 +9,7 @@ from slackeventsapi import SlackEventAdapter
 FILE_PATH = os.path.dirname(os.path.abspath(__file__))
 ASSETS_PATH = os.path.join(FILE_PATH, "assets")
 ENV_PATH = os.path.join(ASSETS_PATH, ".env")
+USEFUL_EVENT_INFO = ["channel", "user", "text"]
 
 # SETUP
 load_dotenv(dotenv_path=ENV_PATH)
@@ -19,6 +20,28 @@ slack_event_adapter = SlackEventAdapter(
 )
 
 client = slack.WebClient(token=os.environ["SLACK_API_TOKEN"])
+
+BOT_USER_ID = client.api_call("auth.test")["user_id"]
+
+
+# HELPER FUNCTIONS
+def send_message(client, channel_id, text):
+    """Sends message of given text to a given channel_id from the given client."""
+
+    client.chat_postMessage(channel=channel_id, text=text)
+
+
+def get_event_info(event, info_to_get):
+    """Returns list containing data from given event object."""
+
+    return [event.get(info) for info in info_to_get]
+
+
+# EVENT HANDLER FUNCTIONS
+@slack_event_adapter.on("message")
+def on_message(payload):
+    event = payload.get("event", {})
+    channel_id, user_id, text = get_event_info(event, USEFUL_EVENT_INFO)
 
 
 if __name__ == "__main__":
