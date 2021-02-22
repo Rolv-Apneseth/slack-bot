a72d251b302479072f42f3f2e23ef02a6cca0e59
Rolv Apneseth <rolv.apneseth@gmail.com>
2021-02-22 18:36:38 +0000

    Added code for running a flask server

--- slack-bot/main.py
+++ slack-bot/main.py
@@ -1,13 +1,25 @@
 import slack
 import os
 from dotenv import load_dotenv
+from flask import Flask
+from slackeventsapi import SlackEventAdapter
+
 
 # CONSTANTS
 FILE_PATH = os.path.dirname(os.path.abspath(__file__))
-ASSETS_PATH = os.path.join(FILE_PATH, 'assets')
-ENV_PATH = os.path.join(ASSETS_PATH, '.env')
+ASSETS_PATH = os.path.join(FILE_PATH, "assets")
+ENV_PATH = os.path.join(ASSETS_PATH, ".env")
 
 # SETUP
 load_dotenv(dotenv_path=ENV_PATH)
 
-client = slack.WebClient(token=os.environ['SLACK_API_TOKEN'])
+app = Flask(__name__)
+slack_event_adapter = SlackEventAdapter(
+    os.environ["SLACK_SIGNING_SECRET"], "/slack/events", app
+)
+
+client = slack.WebClient(token=os.environ["SLACK_API_TOKEN"])
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
