c5bab79fe84e8d5e795f2a7bbc0b33bde259357e
Rolv Apneseth <rolv.apneseth@gmail.com>
2021-02-23 17:15:40 +0000

    Started work on a class to handle welcoming users

---
+++ slack-bot/assets/welcome.py
@@ -0,0 +1,37 @@
+class WelcomeMessage:
+    START_TEXT = {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": (
+                "Welcome to this channel\n\n" "*Get started by completing the tasks*"
+            ),
+        },
+    }
+
+    DIVIDER = {"type": "divider"}
+
+    def __init__(self, channel, user_id):
+        self.channel = channel
+        self.user_id = user_id
+        self.icon_emoji = ":robot_face:"
+        self.timestamp = ""
+        self.is_completed = False
+
+    def get_message(self):
+        return {
+            "ts": self.timestamp,
+            "channel": self.channel,
+            "username": "Welcome bot",
+            "icon_emoji": self.icon_emoji,
+            "blocks": [self.START_TEXT, self.DIVIDER, self._get_reaction_task()],
+        }
+
+    def _get_reaction_task(self):
+        checkmark = ":white_check_mark:"
+        if not self.is_completed:
+            checkmark = ":white_large_square"
+
+        text = f"{checkmark} *React to this message!"
+
+        return {"type": "section", "text": {"type": "mrkdwn", "text": text}}
